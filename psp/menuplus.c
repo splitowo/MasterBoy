05b5b89c1c460e7a8745b2c9937960c0e6a6696b
splitowo <zbrzezinski@protonmail.com>
2024-06-24 23:52:53 +0200

    Version increment 2.02.2

--- psp/menuplus.c
+++ psp/menuplus.c
@@ -3240,7 +3240,7 @@ void DrawBackground()		{
 	//Haut
 	oslSetTextColor(RGB(255,255,255));
 	MyDrawFillRect(0, 0, 480, 12, RGBA(0,0,0,128));
-	oslDrawString(0, -1, "MasterBoy v2.02.1 by Brunni");
+	oslDrawString(0, -1, "MasterBoy v2.02.2 by Brunni");
 	MyDrawLine(0, 12, 480, 12, RGBA(255, 255, 255, 192));
 	//Bas
 	oslSetFont(ftStandard);
